db802f5b89363a162b5dae8828e085056ad3a311
Subhajit <subhabera211@gmail.com>
2023-09-10 13:39:58 +0530

    Update Chart.jsx

--- crypto-tracker/src/components/Chart.jsx
+++ crypto-tracker/src/components/Chart.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+// https://www.chartjs.org/docs/3.9.1/  ->chart.js documentation
 
-const Chart = () => {
+import React from 'react';
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS,CategoryScale, LineController, LineElement, PointElement, LinearScale, Title ,Tooltip,Legend} from "chart.js";
+ChartJS.register(CategoryScale,LineController, LineElement, PointElement, LinearScale, Title,Tooltip,Legend);
+
+const Chart = ({ arr = [], currency, days }) => {
+  const prices = [];
+  const date = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (days === "24h") date.push(new Date(arr[i][0]).toLocaleTimeString());
+    else date.push(new Date(arr[i][0]).toLocaleDateString());
+    prices.push(arr[i][1]);
+  }
+
+  const data = {
+    labels: date,
+    datasets: [
+      {
+        label: `Price in ${currency}`,
+        data: prices,
+        borderColor: "rgb(255,99,132)",
+        backgroundColor: "rgba(255,99,132,0.5)",
+      },
+    ],
+  };
+
+
   return (
-    <div>Chart</div>
+    <Line
+      options={{
+        responsive: true,
+      }}
+      data={data}
+    />
   )
 }
 
